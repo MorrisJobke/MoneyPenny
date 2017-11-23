1984faf4ba2ce23fb93fe8412a3291803f63eb74
Morris Jobke <hey@morrisjobke.de>
2017-11-23 20:59:43 +0100

    Remove HelloWorld route
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import AddExpense from '@/components/AddExpense'
 import ListExpenses from '@/components/ListExpenses'
 
@@ -8,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/add',
       name: 'AddExpense',
